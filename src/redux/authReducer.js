9d9ed4aea12a1f41e550bac57514061a74fcba3b
MaxxMannn <116672250+MaxxMannn@users.noreply.github.com>
2023-07-30 17:30:45 +0200

    re

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -12,25 +12,13 @@ const initialState ={
 
 // -------------------------------------- REGISTER
 
-const register_pending = (state, { payload }) =>{
-
-}
-
 const register_fulfilled = (state, { payload }) =>{
     state.user = payload.user;
     state.token = payload.token;
     state.isLoggedIn = true;
 }
 
-const register_rejected = (state, { payload }) =>{
-
-}
-
 // -------------------------------------- LOG-IN
-
-const logIn_pending = (state, { payload }) =>{
-
-}
 
 const logIn_fulfilled = (state, { payload }) =>{
     state.user = payload.user;
@@ -38,24 +26,12 @@ const logIn_fulfilled = (state, { payload }) =>{
     state.isLoggedIn = true;
 }
 
-const logIn_rejected = (state, { payload }) =>{
-
-}
-
 // -------------------------------------- LOG-OUT
-
-const logOut_pending = (state, { payload }) =>{
-
-}
 
 const logOut_fulfilled = (state, { payload }) =>{
     state.user = { name: null, email: null };
     state.token = null;
     state.isLoggedIn = false;
-}
-
-const logOut_rejected = (state, { payload }) =>{
-
 }
 
 // -------------------------------------- REFRESH
